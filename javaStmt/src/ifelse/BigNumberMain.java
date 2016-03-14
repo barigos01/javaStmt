462b3aad772d350fec66448282287df1b8da4ccc
강은지 <barigos62@naver.com>
2016-03-14 10:04:22 +0900

    .

--- javaStmt/src/ifelse/BigNumberMain.java
+++ javaStmt/src/ifelse/BigNumberMain.java
@@ -8,14 +8,22 @@ import java.util.Scanner;
  * @date 2016. 3. 11.
  * @story 입력된 3개 숫자에서 가장 큰 수
  */
-public class BigNumber {
+public class BigNumberMain {
 	public static void main(String[] args) {
 		System.out.println("3개의 숫자를 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
 		int a=scanner.nextInt(), b=scanner.nextInt(), c=scanner.nextInt();
 		int big=0;
 		
-		if (a>b) {
+		if(a>b || a>c){
+			big = a;
+		} else if(b>c){
+			big = b;
+		} else{
+			big = c;
+		}
+		
+/*		if (a>b) {
 			if (a>c) {
 				big = a;
 			} else{
@@ -28,9 +36,11 @@ public class BigNumber {
 				big = c;
 			}
 			
-		}
+		}*/
 		
 		System.out.println("가장 큰 수는 "+big+"입니다.");
 		
 	}
 }
+
+class BigNumber{}
