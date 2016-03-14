1ba5ee600ed89c654852f83a4a48eef9ecb64c06
강은지 <barigos62@naver.com>
2016-03-14 17:50:29 +0900

    .

---
+++ javaStmt/src/grade/GradeService.java
@@ -0,0 +1,28 @@
+package grade;
+/**
+ * CRUD
+ * C : create	생성
+ * R : read		조회
+ * U : update	수정
+ * D : delete	삭제
+ * */
+public interface GradeService {
+	// C : 성적표 출력
+	public void input();	// 추상 메소드 (기능을 추상화한다. 속성은 추상화하지않는다.)
+	
+	// R : 성적표 리스트 출력
+	public void getList();
+	
+	// R : 성적표 조회(학번)
+	public void getGradeByHak();
+	
+	// R : 성적표 조회(이름)
+	public void getGradeByName();
+	
+	// U : 성적표 수정
+	public void update();
+	
+	// D : 성적표 삭제
+	public void delete(); 
+	
+}
