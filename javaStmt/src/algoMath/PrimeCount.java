ce62bf84a9e1ef1fe7bab1763740e3d874f30150
강은지 <barigos62@naver.com>
2016-03-24 12:28:57 +0900

    .

--- javaStmt/src/algoMath/PrimeCount.java
+++ javaStmt/src/algoMath/PrimeCount.java
@@ -8,7 +8,26 @@ package algoMath;
 public class PrimeCount {
 
 	public static void main(String[] args) {
+		int[] A = new int[99];
+		int K = 1, i = 0, J = 0, M = 0;
 		
+		while (K >= 100) {
+			K = K+1;
+			A[K-1] += K;
+			
+			i = i+1;
+			
+			if (i > 99) {
+				if (A[i] == 0) {
+					J++;
+					M = i;
+				}
+			} else {
+
+			}
+			
+			K++;
+		}
 
 	}
 
